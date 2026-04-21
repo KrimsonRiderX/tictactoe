b3df05136a8c8ba5d3e36c53f2631addf4a0e700
KrimsonRiderX <adityaojha2004@gmail.com>
2026-04-21 15:18:26 +0530

    random

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,9 +1,42 @@
+import java.util.Random;
+
 public class tictactoe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSybmol;
+
+
     static char[][] board = new char[3][3];
 
     public static void main (String[] Args){
+        tossAndAssignSymobols();
+        displayTossResults();
         initializeboard();
         printboard();
+    }
+
+    static void tossAndAssignSymobols(){
+        Random rand = new Random();
+
+        if (rand.nextInt()%2 == 0){
+            humanSymbol = 'X';
+            computerSybmol = 'O';
+        }
+        else {
+            humanSymbol = 'O';
+            computerSybmol = 'X';
+        }
+    }
+
+    static void displayTossResults(){
+
+        if (humanSymbol == 'X'){
+            System.out.println("you won the toss move first");
+        }
+        else {
+            System.out.println("you lost the toss computer move first");
+        }
     }
 
     static void initializeboard(){
